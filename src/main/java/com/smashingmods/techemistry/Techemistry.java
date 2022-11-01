be52aa5485a85c1d2385ee9f117aef0d67811ba7
Andrew Sampson <andrew@akliz.net>
2022-11-01 18:14:00 -0500

    Initial commit.

---
+++ src/main/java/com/smashingmods/techemistry/Techemistry.java
@@ -0,0 +1,16 @@
+package com.smashingmods.techemistry;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(Techemistry.MODID)
+public class Techemistry {
+
+    public static final String MODID = "techemistry";
+    @SuppressWarnings("unused")
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Techemistry() {
+    }
+}
